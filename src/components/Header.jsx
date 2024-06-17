2f1c3e8724120ee8c8d11400a58cefdf8c8fb44c
diego vallejo <vallejodiego@live.com>
2024-06-17 15:15:11 -0300

    logout funcional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import Anchor from './Anchor'
 import { navAnchorArray, clientAnchorArray } from '../utils/anchorArray.js'
-import { useSelector } from 'react-redux'
-import { logout } from '../redux/actions/authActions.js'
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout } from '../redux/actions/authActions.js';
+import { Button } from 'flowbite-react'
+
 const Header = () => {
 
-  const user = useSelector(store => store.auth);
-  console.log(user)
   const dispatch = useDispatch();
-  const clientLogout = (name) => {
-    if (name === "Logout") {
-      dispatch(logout());
-      console.log(user);
-    }
+  const user = useSelector(store => store.auth);
+  const isLoggedIn = user.loggedIn;
+
+  const clientLogout = () => {
+    dispatch(logout());
   }
-  const isLoggedIn = user.loggedIn
-  console.log(isLoggedIn)
+
   return (
     <>
       <header className="w-full min-h-[10vh]  bg-[#2c3e50] flex flex-col flex-wrap items-center justify-between gap-2 pt-3 lg:pt-0 lg:flex-row">
         <div className="w-full lg:w-1/3 h-full flex items-center justify-center ">
           <img className="w-[250px]" src="/img/wolfPng.png" alt="logo" />
-        </div>
+        </div>{isLoggedIn && <Button className='bg-[#AB9144] sm:self-end xl:mt-4 xl:mr-8  mr-[10px]' size="xs" onClick={clientLogout}>Logout</Button>}
         <nav className="flex flex-wrap w-full md:w-3/5 md:justify-center md:gap-[2px] lg:self-end lg:justify-end lg:gap-1 lg:w-3/5" >
-          {isLoggedIn ? clientAnchorArray.map((link) => <Anchor key={link.name} name={link.name} theRef={link.theRef} clickHandler={() => clientLogout(link.name)} />) : navAnchorArray.map((link) => <Anchor key={link.name} name={link.name} theRef={link.theRef} clickHandler={() => clientLogout(link.name)} />)}
+          {isLoggedIn ? clientAnchorArray.map((link) => <Anchor key={link.name} name={link.name} theRef={link.theRef} />) : navAnchorArray.map((link) => <Anchor key={link.name} name={link.name} theRef={link.theRef} />)}
         </nav>
       </header>
     </>
--- src/utils/anchorArray.js
+++ src/utils/anchorArray.js
@@ -1,6 +1,6 @@
 export const navAnchorArray = [{ theRef: "/", name: "Home" }, { theRef: "/register", name: "Register" }, { theRef: "/login", name: "Login" }];
 
 
-export const clientAnchorArray = [{ theRef: "/clientHome", name: "Home" }, { theRef: "/clients/account", name: "Account" }, { theRef: "/clients/card", name: "Card" }, { theRef: "/clients/transaction", name: "Transactions" }, { theRef: "/clients/loan", name: "Loans" }, { theRef: "/", name: "Logout" }];
+export const clientAnchorArray = [{ theRef: "/clientHome", name: "Home" }, { theRef: "/clients/account", name: "Account" }, { theRef: "/clients/card", name: "Card" }, { theRef: "/clients/transaction", name: "Transactions" }, { theRef: "/clients/loan", name: "Loans" }];
 
 export default { navAnchorArray, clientAnchorArray }
