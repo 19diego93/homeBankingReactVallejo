6f44d47b5a50974f6dd83d4f2f48a45905c144d6
diego vallejo <vallejodiego@live.com>
2024-06-19 02:35:15 -0300

    reformas home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,7 +50,7 @@ const Home = () => {
             <section className='flex flex-col items-center justify-center mt-4 lg:mt-10 w-[95%] bg-white rounded-xl shadow-lg p-10' >
                 <h3 className="text-2xl font-semibold tracking-tight text-gray-900 bg-[#add8e6] py-3 px-7 rounded-lg">About Us</h3>
                 <div className="flex flex-col justify-center items-center lg:flex-row mt-10 gap-5 w-full ">
-                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/eCommerce3.png" horizontal>
+                    <Card className="w-[95%] lg:w-[45%] bg-[#add8e6]" imgSrc="../../img/eCommerce3.png" horizontal>
                         <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
                             WolfBank Offers Competitive Loan and Credit Services
                         </h5>
@@ -66,7 +66,7 @@ const Home = () => {
                             WolfBank is excited to introduce a new suite of financial planning tools designed to help you achieve your financial goals. Our tools offer personalized budgeting assistance, savings goal trackers, and investment advice tailored to your unique needs.
                         </p>
                     </Card>
-                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/aiBank2.png" horizontal>
+                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/iaBank1.png" horizontal>
                         <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
                             WolfBank Recognized for Excellence in Customer Support
                         </h5>
@@ -74,7 +74,7 @@ const Home = () => {
                             We are proud to announce that WolfBank has received the "Excellence in Customer Support" award from the Financial Services Association. This award highlights our dedication to providing exceptional 24/7 customer service.
                         </p>
                     </Card>
-                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/aiBank1.png" horizontal>
+                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/aiBank2.jpg" horizontal>
                         <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
                             WolfBank Named "Most Innovative Bank" of the Year
                         </h5>
@@ -86,6 +86,7 @@ const Home = () => {
                 </div>
             </section>
             <section className="flex flex-col items-center justify-center mt-4 lg:mt-10 w-[95%] bg-white rounded-xl shadow-lg p-10">
+                <h3 className="text-2xl font-semibold tracking-tight text-gray-900 bg-[#add8e6] py-3 px-7 rounded-lg">Contact Us</h3>
                 <form className="flex flex-col gap-4 mt-4" onSubmit={handleSubmit}>
                     <input type="email"
                         placeholder="Email"
