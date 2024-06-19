43f82ce38e28f8fc8e58d3aba94c205e25ebe949
diego vallejo <vallejodiego@live.com>
2024-06-19 11:57:11 -0300

    amount del account elegida

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -67,14 +67,14 @@ const Transactions = () => {
                 <form className="space-y-6">
                     <div className="flex flex-col">
                         <label htmlFor="account" className="text-sm font-medium">Accounts</label>
-                        {selectedAccount && <p>Balance: ${selectedAccount.balance}</p>}
+                        {selectedAccount && <p>Balance: ${accounts.find(account => account.number === selectedAccount).balance}</p>}
                         <select
                             id="account"
                             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             value={selectedAccount}
                             onChange={(e) => setSelectedAccount(e.target.value)}
                         >
-                            <option disabled>Choose one</option>
+                            <option value="" disabled>Choose one</option>
                             {accounts.map((account) => (
                                 <option key={account.id} value={account.number}>{account.number}</option>
                             ))}
