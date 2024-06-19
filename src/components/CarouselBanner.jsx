e15b9af14562d02337579d40ef96bb73ee284e5e
diego vallejo <vallejodiego@live.com>
2024-06-19 02:22:42 -0300

    img y home variacion

--- src/components/CarouselBanner.jsx
+++ src/components/CarouselBanner.jsx
@@ -4,10 +4,10 @@ const CarouselBanner = ({ widthHeight }) => {
   return (
     <div className={widthHeight}>
       <Carousel slideInterval={5000} pauseOnHover>
-        <img src="../../public/img/bank-finance-horizontal-sale-banner-template_23-2149726028.jpg" alt="1" />
-        <img src="../../public/img/bank-service-concept-facebook-cover-template_23-2150459496.jpg" alt="2" />
-        <img src="../../public/img/illustration-of-banking-banking-equipment-concept-modern-bank-reception-office-interior-horizontal-banner-vector.jpg" alt="3" />
-        <img src="../../public/img/bank-service-concept-sale-banner-template_23-2150487020.jpg" alt="4" />
+        <img src="../../img/bank-finance-horizontal-sale-banner-template_23-2149726028.jpg" alt="1" />
+        <img src="../../img/bank-service-concept-facebook-cover-template_23-2150459496.jpg" alt="2" />
+        <img src="../../img/illustration-of-banking-banking-equipment-concept-modern-bank-reception-office-interior-horizontal-banner-vector.jpg" alt="3" />
+        <img src="../../img/bank-service-concept-sale-banner-template_23-2150487020.jpg" alt="4" />
       </Carousel>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,84 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CarouselBanner from '../components/CarouselBanner';
 import { Card } from 'flowbite-react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Home = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const { email, message } = formData;
+
+        if (email && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) && message) {
+            toast.success('Message sent!');
+            setFormData({
+                email: '',
+                message: '',
+            });
+        } else {
+            toast.error('Message not sent!');
+        }
+    };
+
     return (
         <div className="flex flex-wrap flex-col items-center gap-4">
             <h1 className="text-center font-bold pt-5 text-4xl">Welcome to Wolfbank</h1>
 
             <section className="w-[95%] bg-white rounded-xl shadow-lg p-10 mt-10 flex flex-col items-center justify-center md:flex-row md:justify-between">
                 <div className="w-full md:w-3/4">
-                    <h2 className="text-center text-2xl bg-[#add8e6] p-3 rounded-lg font-semibold">Our Services</h2>
-                    <p className="text-center text-lg mt-5">At WolfBank, we believe in making banking easy, secure, and convenient for you. Our home banking services are designed to bring the bank to your fingertips, offering a comprehensive range of financial solutions from the comfort of your home.</p>
+                    <h2 className="text-center text-2xl bg-[#add8e6] p-3 rounded-lg font-semibold">
+                        Our Services
+                    </h2>
+                    <p className="text-center text-lg mt-5">
+                        At WolfBank, we believe in making banking easy, secure, and
+                        convenient for you. Our home banking services are designed to bring
+                        the bank to your fingertips, offering a comprehensive range of
+                        financial solutions from the comfort of your home.
+                    </p>
                     <ul className="list-disc mt-5 pl-5 mx-auto w-[66%] ">
-                        <li>Create accounts <span className="text-xs">- Apply for an account</span></li>
-                        <li>Transactions <span className="text-xs">- Transfer funds, pay bills, etc.</span></li>
-                        <li>Get cards <span className="text-xs">- Apply for a card for online purchases</span></li>
-                        <li>Loans <span className="text-xs">- Apply for a loan to cover unexpected expenses</span></li>
+                        <li>
+                            Create accounts <span className="text-xs">- Apply for an account</span>
+                        </li>
+                        <li>
+                            Transactions <span className="text-xs">- Transfer funds, pay bills, etc.</span>
+                        </li>
+                        <li>
+                            Get cards <span className="text-xs">- Apply for a card for online purchases</span>
+                        </li>
+                        <li>
+                            Loans <span className="text-xs">- Apply for a loan to cover unexpected expenses</span>
+                        </li>
                     </ul>
                 </div>
-                <div className=" w-full md:w-3/5">
+                <div className="w-full md:w-3/5">
                     <img className="w-full h-full" src="../../img/eCommerce2.png" alt="wolfbank" />
                 </div>
             </section>
-            <section className='flex flex-col items-center justify-center mt-4 lg:mt-10 w-[95%] bg-white rounded-xl shadow-lg p-10' >
-                <h3 className="text-2xl font-semibold tracking-tight text-gray-900 bg-[#add8e6] py-3 px-7 rounded-lg">About Us</h3>
+            <section className="flex flex-col items-center justify-center mt-4 lg:mt-10 w-[95%] bg-white rounded-xl shadow-lg p-10">
+                <h3 className="text-2xl font-semibold tracking-tight text-gray-900 bg-[#add8e6] py-3 px-7 rounded-lg">
+                    About Us
+                </h3>
                 <div className="flex flex-col justify-center items-center lg:flex-row mt-10 gap-5 w-full ">
-                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/eCommerce3.png" horizontal>
-                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
-                            WolfBank Offers Competitive Loan and Credit Services
-                        </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">
-                            WolfBank is now offering a range of competitive loan and credit services to meet your financial needs. Whether you're looking for a personal loan, mortgage, or business credit, our streamlined application process and attractive rates make borrowing easy. Visit our website to learn more and apply online today.
-                        </p>
-                    </Card>
-                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/eCommerce1.png" horizontal>
-                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
-                            WolfBank Launches Innovative Financial Planning Tools
-                        </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">
-                            WolfBank is excited to introduce a new suite of financial planning tools designed to help you achieve your financial goals. Our tools offer personalized budgeting assistance, savings goal trackers, and investment advice tailored to your unique needs.
-                        </p>
-                    </Card>
-                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/eCommerce3.png" horizontal>
-                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
-                            WolfBank Recognized for Excellence in Customer Support
-                        </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">
-                            We are proud to announce that WolfBank has received the "Excellence in Customer Support" award from the Financial Services Association. This award highlights our dedication to providing exceptional 24/7 customer service.
-                        </p>
-                    </Card>
-                    <Card className="w-[95%] bg-[#add8e6]" imgSrc="../../img/eCommerce1.png" horizontal>
-                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
-                            WolfBank Named "Most Innovative Bank" of the Year
-                        </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">
-                            WolfBank has been honored with the "Most Innovative Bank" award by the International Finance Awards 2024. This recognition celebrates our commitment to bringing cutting-edge technology and innovative banking solutions to our customers.
-
-                        </p>
-                    </Card>
+                    {/* Cards */}
                 </div>
             </section>
             <section className="flex flex-col items-center justify-center mt-4 lg:mt-10 w-[95%] bg-white rounded-xl shadow-lg p-10">
-                <h3 className="text-2xl font-semibold tracking-tight text-gray-900 bg-[#add8e6] py-3 px-7 rounded-lg">Contact Us</h3>
-                <div className="flex flex-col items-center justify-center lg:flex-row mt-10 gap-5 w-full">
-                    <div className="w-full lg:w-3/5">
-                        <h4 className="text-xl font-semibold tracking-tight text-gray-900">Send us a message</h4>
-                        <form className="flex flex-col gap-4 mt-4">
-                            <input type="text" placeholder="Email" className="px-4 py-2 border border-gray-700 rounded-lg" />
-                            <textarea placeholder="Message" rows={5} className="px-4 py-2 border border-gray-700 rounded-lg"></textarea>
-                            <button onClick={() => alert("Mensaje enviado")} className="px-4 py-2 bg-blue-600 text-white rounded-lg">Send</button>
-                        </form>
-                    </div>
-                </div>
+                <form className="flex flex-col gap-4 mt-4" onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        className="px-4 py-2 border border-gray-700 rounded-lg"
+                    />
+                    <textarea
+                        placeholder="Message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        rows={5}
+                        className="px-4 py-2 border border-gray-700 rounded-lg"
+                    ></textarea>
+                    <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-lg">
+                        Send
+                    </button>
+                </form>
             </section>
 
-
-            <div className='my-5 p-5 rounded-lg flex justify-center w-full h-[300px]'>
-                <CarouselBanner widthHeight={"w-[90%] h-[90%]"} />
+            <div className="my-5 p-5 rounded-lg flex justify-center w-full h-[300px]">
+                <CarouselBanner widthHeight={'w-[90%] h-[90%]'} />
             </div>
+            <ToastContainer theme="colored" position="bottom-right" />
         </div>
-    )
-}
+    );
+};
 
 export default Home;
